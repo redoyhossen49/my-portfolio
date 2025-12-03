6ce03ed1f12d059f00145e10a7713cb7512d2ceb
Md Redoy Hossen <redoykhan01943147885@gmai.com>
2025-12-03 15:51:58 +0700

    All page background color change & layout updated

--- src/layout/MainLayOut.jsx
+++ src/layout/MainLayOut.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import CustomCursor from "../components/CustomCursor";
 
 const MainLayOut = () => {
   return (
-    <div>
+    <div className="cursor-none">
+      <CustomCursor></CustomCursor>
       <NavBar></NavBar>
-      <Outlet></Outlet>
+      <div
+        className=" bg-gray-800  [background-image:linear-gradient(#333_1px,transparent_1px),linear-gradient(90deg,#333_1px,transparent_1px)] 
+        [background-size:100px_100px] "
+      >
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
